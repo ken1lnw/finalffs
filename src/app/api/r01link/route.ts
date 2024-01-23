9bc69650024f0161208c44704fef46e910282a64
ken1lnw <moowinza@gmail.com>
2024-01-23 15:31:02 +0700

    comment api

--- src/app/api/r01link/route.ts
+++ src/app/api/r01link/route.ts
@@ -10,22 +10,25 @@ import { NextResponse } from 'next/server';
 
 
 // }
-export async function GET(request:NextApiRequest) {
+
+
+
+// export async function GET(request:NextApiRequest) {
  
-  // external file URL
-    const pdfUrl = 'https://dev.cpe.rmuti.ac.th/fss/rfile/r01.pdf';
+//   // external file URL
+//     const pdfUrl = 'https://dev.cpe.rmuti.ac.th/fss/rfile/r01.pdf';
 
-    // use fetch to get a response
-    const response = await fetch(pdfUrl);
+//     // use fetch to get a response
+//     const response = await fetch(pdfUrl);
 
-    // return a new response but use 'content-disposition' to suggest saving the file to the user's computer
-    return new Response(response.body, {
-        // responseType: 'arraybuffer',
-        headers: {
-            'Content-Type': 'application/pdf',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-            'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-        },
-    });
-}
+//     // return a new response but use 'content-disposition' to suggest saving the file to the user's computer
+//     return new Response(response.body, {
+//         // responseType: 'arraybuffer',
+//         headers: {
+//             'Content-Type': 'application/pdf',
+//             'Access-Control-Allow-Origin': '*',
+//             'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+//             'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+//         },
+//     });
+// }
--- src/app/request/r01/page.tsx
+++ src/app/request/r01/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { useState } from "react";
 import PrintR01 from "./print/page";
 
